79d6b5c8032b7842a519886772c05f1511d9c93a
hulsizer <andrew.hulsizer@gmail.com>
2020-12-09 13:58:34 -0600

    Final Day 8

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -14,7 +14,7 @@ pub fn pt1(input: std::string::String) {
     while instruction_row < code.len() {
         
         if instruction_set.contains(&instruction_row) {
-            println!("Loop! Accumulator: {}", accumulator);
+            println!("Loop! Unwind");
             break;
         } else {
             instruction_set.insert(instruction_row);
@@ -61,10 +61,10 @@ pub fn pt2(input: std::string::String) {
     let mut instruction_set = HashSet::<usize>::new();
     
     //From my understanding this is on the stack (&str)
-    let mut code: Vec<&str> = input.lines().collect();
+    let mut code: Vec<String> = input.lines().map(|line| line.to_string() ).collect();
 
     let mut instruction_row: usize = 0; 
-    let mut instruction_stack = Vec::<(usize, usize, HashSet::<usize>, &str)>::new();
+    let mut instruction_stack = Vec::<(usize, usize, HashSet::<usize>, String)>::new();
 
     let mut collect_operations = true;
 
@@ -73,20 +73,22 @@ pub fn pt2(input: std::string::String) {
         if instruction_set.contains(&instruction_row) {
             collect_operations = false;
             println!("Loop! Accumulator: {}", accumulator);
+
             match instruction_stack.pop() {
                 Some(operation) => {
                     accumulator = operation.0;
                     instruction_row = operation.1;
                     instruction_set = operation.2;
-                    let instruction = code[instruction_row];
-                    match code[instruction_row] {
+                    
+                    let components: Vec<String> = code[instruction_row].split(" ").map(|line| line.to_string() ).collect();
+                    match components[0].as_str() {
                         "jmp" => { 
-                            let together = format!("nop {}", instruction.chars().as_str());
-                            code[instruction_row] = &together;
+                            let together = format!("nop {}", components[1].as_str());
+                            code[instruction_row] = together;
                         },
                         "nop" => { 
-                            let together = format!("nop {}", instruction.chars().as_str());
-                            code[instruction_row] = &together;
+                            let together = format!("nop {}", components[1].as_str());
+                            code[instruction_row] = together;
                         },
                         _ => (),
                     }
@@ -97,16 +99,18 @@ pub fn pt2(input: std::string::String) {
             instruction_set.insert(instruction_row);
         }
 
-        let components: Vec<&str> = code[instruction_row].split(" ").collect();
-        match components[0] {
+        let components: Vec<String> = code[instruction_row].split(" ").map(|line| line.to_string() ).collect();
+        match components[0].as_str() {
             "acc" => {
                 let mut chars = components[1].chars();
                 match chars.next().unwrap() {
                     '+' => {
                         accumulator += chars.as_str().parse::<usize>().unwrap();
+                        //println!("During {:?}", accumulator);
                     },
                     '-' => {
                         accumulator -= chars.as_str().parse::<usize>().unwrap();
+                        //println!("During {:?}", accumulator);
                     }
                     _ => ()
                 }
@@ -114,13 +118,13 @@ pub fn pt2(input: std::string::String) {
             },
             "nop" => {
                 if collect_operations {
-                    instruction_stack.push((accumulator, instruction_row, instruction_set.iter().copied().collect(), components[1]));
+                    instruction_stack.push((accumulator, instruction_row, instruction_set.iter().copied().collect(), components[1].clone()));
                 }
                 instruction_row += 1;
             },
             "jmp" => {
                 if collect_operations {
-                    instruction_stack.push((accumulator, instruction_row, instruction_set.iter().copied().collect(), components[1]));
+                    instruction_stack.push((accumulator, instruction_row, instruction_set.iter().copied().collect(), components[1].clone()));
                 }
                 let mut chars = components[1].chars();
                 match chars.next().unwrap() {
@@ -137,4 +141,6 @@ pub fn pt2(input: std::string::String) {
             _ => ()
         }
     }
+
+    println!("Accumulator {}", accumulator)
 }
